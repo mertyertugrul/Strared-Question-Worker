457c2e5b1b6380d9a650d490497289e5c005b7e1
Mertugrul <merty.ertugrul@gmail.com>
2024-01-12 01:19:17 +0000

    Initial commit without IDEA project files

---
+++ src/main/java/model/Question.java
@@ -0,0 +1,19 @@
+package question;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class Question {
+    private String id;
+    private String note;
+    private int timesSolved;
+
+    public Question(String id, String note) {
+        this.id = id;
+        this.note = note;
+        this.timesSolved = 0;
+    }
+    
+}
